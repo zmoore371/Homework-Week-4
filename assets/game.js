66570f198582356314131ba8f7ffbea9d63fa829
Zmoore <zmoore371@gmail.com>
2021-08-27 15:22:52 -0400

    now able to store string number from button click. need to convert it to a number and then use that to check the right answer. line 65 and 66 make an infinte loop right now. oops

--- assets/game.js
+++ assets/game.js
@@ -5,8 +5,11 @@ var answer1 = $(".choice-text1");
 var answer2 = $(".choice-text2");
 var answer3 = $(".choice-text3");
 var answer4 = $(".choice-text4");
-
-
+var button = $(".choice-container")
+// var button1 = $('.choice-container1');
+// var button2 = $('.choice-container2');
+// var button3 = $('.choice-container3');
+// var button4 = $('.choice-container4');
 
 
 const myQuestions = [
@@ -22,22 +25,16 @@ const myQuestions = [
     }//PUT A COMMA HER FOR THE NEXT QUESTION DINGUS
 ]
 maxQuestions = 4
-
+questionNumber = 0 //if i increment this by 1 it will change the question and answer choices
 
 
 function writeQustion() {
-    
-    for (i=0; i<maxQuestions; i++) {
-    question.text(myQuestions[0].question)
-    answer1.text(myQuestions[0].answers[0])
-    answer2.text(myQuestions[0].answers[1])
-    answer3.text(myQuestions[0].answers[2])
-    answer4.text(myQuestions[0].answers[3])
-    }
-
-    
-   
-    
+    question.text(myQuestions[questionNumber].question)
+    answer1.text(myQuestions[questionNumber].answers[0])
+    answer2.text(myQuestions[questionNumber].answers[1])
+    answer3.text(myQuestions[questionNumber].answers[2])
+    answer4.text(myQuestions[questionNumber].answers[3])
+    checkRightAnswer();
 }
 
 
@@ -46,29 +43,44 @@ function getNewQuestion() {
 
 }
 
+
+
+
+function checkRightAnswer() {
+    let ans = $("button").click(function() {
+        var fired_button = $(this).val();
+        alert(fired_button);
+        console.log(fired_button)
+        console.log(typeof fired_button)
+    });
+    ansNum = +ans
+
+    // if (ans !== myQuestions[questionNumber].correctAnswer) {
+    //     console.log('Hello')
+    //     questionNumber + 1
+
+    // }
+
+    if (isNaN(ansNum) === true){
+        console.log('hiya')
+        checkRightAnswer(); //oops
+    } else {
+        console.log(ansNum)
+    }
+
+
+
+    
+
+}
+
 writeQustion();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 function init(){
     secondsLeft = 15;
 
     questionCounter = 0
     score = 0 
-    avaliableQuestions = [...myQuestions]
     getNewQuestion();
     
     startTimer();
@@ -90,3 +102,34 @@ function startTimer(){
 }
 
 init();
+
+
+// button.click(function(){
+//     answerChoice = this.button.text();
+//     console.log(answerChoice) 
+// })
+
+// var buttonResult = $(function(){
+//     $("button").click(function() {
+//         var fired_button = $(this).val();
+//         alert(fired_button);
+//     });
+// });
+
+// console.log(buttonResult);
+
+// let ans = $("button").click(function() {
+//     var fired_button = $(this).val();
+//     alert(fired_button);
+//     console.log(fired_button)
+//     console.log(typeof fired_button)
+// });
+
+// console.log(fired_button)
+
+// using the button method above you are able to store the number of the button clicked. now we need to figure out how to turn it from a sring to an interger and compare it to the correct answer under each question; lunchtime 
+
+
+var abc = "16"
+var abcd = +abc
+console.log(typeof abcd)
