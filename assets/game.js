c1e27dabe2e1739daf5e46ff0894d0eb541531c7
Zmoore <zmoore371@gmail.com>
2021-08-30 15:11:01 -0400

    deleted original JS file that was not needed. Quiz now runs through and creates local storage if ther is none in the first place or adds onto the existing array if it does exist. the reset button on the highscore/ leaderboard page clears the local storage of all existing scores. The list does not sort by value, last thing to tackle as well as a README

--- assets/game.js
+++ assets/game.js
@@ -1,5 +1,4 @@
 var timeEl = $(".timer");
-// var highscoreForm = $('.highscore-input')
 var initialsForm = $('.initials-form')
 var yourScore = $('.your-score-was')
 var question = $("#question");
@@ -10,28 +9,48 @@ var answer4 = $(".choice-text4");
 var button = $(".choice-container");
 var correct = $(".correct");
 var quizBox = $(".quiz-box");
-
+var initialsInput = $('#input-text')
+var highscoreList = []
 
 const myQuestions = [
     {
-        question: "Inside which HTML element do we put the JavaScript?",
-        answers: ["<scripting>","<js>", "<script>","<javascript>"],
-        correctAnswer: 3,
-    },
-    {    
-        question: "What method returns the first index at which a given element can be found in an array, or -1 if it is not present?",
-        answers: ["getIndex()", "location()", "indexOf()","None of the above"],
+        question: "Inside which HTML element do we put JavaScript?",
+        answers: ["<scripting>", "<js>", "<script>", "<javascript>"],
         correctAnswer: 3,
     },
     {
-        question: "What method tests whether at least one element in the array passes the test implemented by the provided function. It returns true if, in the array, it finds an element for which the provided function returns true; otherwise it returns false. ?",
+        question: "What method returns the first index at which a given element can be found in an array, or -1 if it is not present?",
+        answers: ["getIndex()", "location()", "indexOf()", "None of the above"],
+        correctAnswer: 3,
+    },
+    {
+        question: "What method tests whether at least one element in the array passes the test implemented by the provided function. It returns true if, in the array, it finds an element for which the provided function returns true; otherwise it returns false?",
         answers: ["reverse()", "shift()", "slice()", "some()"],
         correctAnswer: 4,
-    }
-        
-        //PUT A COMMA HER FOR THE NEXT QUESTION DINGUS
+    },
+    {
+        question: "What array function adds or removes elements from an array?",
+        answers: ["unshift()", "sort()", "toSource()", "splice()"],
+        correctAnswer: 4,
+    },
+    {
+        question: "When a user views a page containing a JavaScript program, which machine actually executes the script?",
+        answers: ["The User's machine running a Web browser", "The Web server", "A central machine deep within Netscape's corporate offices", "None of the above"],
+        correctAnswer: 1,
+    },
+    {
+        question: "What are variables used for in JavaScript Programs?",
+        answers: ["Storing numbers, dates, or other values", "Varying randomly", "Causing high-school algebra flashbacks", "None of the above"],
+        correctAnswer: 1,
+    },
+    {
+        question: "Which of the following best describes JavaScript?",
+        answers: ["a low-level programming language.", "a scripting language precomplied in the browser.", "a complied scripting language", "an object-oriented scripting language"],
+        correctAnswer: 4,
+    } //PUT A COMMA HER FOR THE NEXT QUESTION DINGUS
+
 ]
-maxQuestions = 3 // change this number to max num questions, could also probably compare to array length but this seems easier for now
+maxQuestions = 7 // change this number to max num questions, could also probably compare to array length but this seems easier for now
 questionNumber = 0 //if i increment this by 1 it will change the question and answer choices
 userScore = 0
 
@@ -46,92 +65,94 @@ function writeQustion() {
 button.click(userAnswer())
 
 function userAnswer() {
-    $("button").click(function() {
+    $("button").click(function () {
         var fired_button = $(this).val();
-        var isCorrect = +fired_button 
+        var isCorrect = +fired_button
         rightAnswer = myQuestions[questionNumber].correctAnswer
-        if(rightAnswer === isCorrect){
-            userScore = userScore+100
+        if (rightAnswer === isCorrect) {
+            userScore = userScore + 100
             console.log(userScore)
-            nextQuestion(); 
+            nextQuestion();
             correct.show();
-            setTimeout(function() {correct.hide(); }, 1000)
+            setTimeout(function () { correct.hide(); }, 1000)
             return userScore;
-            
-        
+
+
         } else {
             nextQuestion();
         }
     });
 }
-    
-function nextQuestion(){
+
+function nextQuestion() {
     questionNumber++
-    
-    if(questionNumber < maxQuestions){
-       writeQustion();
+
+    if (questionNumber < maxQuestions) {
+        writeQustion();
     } else {
         console.log('no more questions')
         endGame();
     }
-    
+
 }
 
-function endGame() { 
+function endGame() {
     quizBox.remove();
     initialsForm.show();
-    yourScore.text("Congats! Your score was: " + userScore)
+    yourScore.text("Congrats! Your score was: " + userScore)
     clearInterval(timer);
     timeEl.hide();
-    
+
 }
 
-function init(){
-    secondsLeft = 10000;
+
+
+function init() {
+    secondsLeft = 180;
     questionCounter = 0
-    score = 0 
+    score = 0
     startTimer();
     writeQustion();
     initialsForm.hide();
     correct.hide();
+    var storedScores = JSON.parse(localStorage.getItem("Scores"))
+    if (storedScores !== null) {
+        highscoreList = storedScores
+    }
 }
 
-function startTimer(){
-    timer = setInterval(function() {
-      secondsLeft--;
-      timeEl.text('Timer: ' + secondsLeft);
-      
-      if(secondsLeft === 0) {
-        clearInterval(timer);
-        endGame();
-      }
+function startTimer() {
+    timer = setInterval(function () {
+        secondsLeft--;
+        timeEl.text('Timer: ' + secondsLeft);
+
+        if (secondsLeft === 0) {
+            clearInterval(timer);
+            endGame();
+        }
     }, 1000);
 }
-
-init();
-
-var initialsInput = $('#input-text')
-var highscoreList = JSON.parse(localStorage.getItem("Scores"));
 
 function storeInitials() {
     localStorage.setItem("Scores", JSON.stringify(highscoreList))
 }
 
-initialsForm.on("submit", function(event) {
+initialsForm.on("submit", function (event) {
     event.preventDefault();
     var initialsText = initialsInput.val();
     console.log(initialsText)
     localStorage.getItem("Scores")
-    if(initialsText === "") {
+    if (initialsText === "") {
         return;
     }
-    highscoreList.push([initialsText,userScore]);
-    console.log('Hello')
+
+    highscoreList.push([initialsText, userScore]);
+
     storeInitials();
-    window.location.href="./highscores.html"
+    window.location.href = "./highscores.html"
 })
 
-// var storedScores = JSON.parse(localStorage.getItem("Scores"));
+init();
 
 
 
@@ -142,79 +163,3 @@ initialsForm.on("submit", function(event) {
 
 
 
-
-// saveButton.on("click", function(event) {
-//     event.preventDefault();
-//     saveHighScore();
-//     console.log('hello')
-// })
-
-// button.click(function(){
-//     answerChoice = this.button.text();
-//     console.log(answerChoice) 
-// })
-
-// var buttonResult = $(function(){
-//     $("button").click(function() {
-//         var fired_button = $(this).val();
-//         alert(fired_button);
-//     });
-// });
-
-// console.log(buttonResult);
-
-// let ans = $("button").click(function() {
-//     var fired_button = $(this).val();
-//     alert(fired_button);
-//     console.log(fired_button)
-//     console.log(typeof fired_button)
-// });
-
-// console.log(fired_button)
-
-// using the button method above you are able to store the number of the button clicked. now we need to figure out how to turn it from a sring to an interger and compare it to the correct answer under each question; lunchtime 
-
-
-// var abc = "16"
-// var abcd = +abc
-// console.log(typeof abcd)  PROOF OF CONCEPT
-// console.log(typeof abc)
-
-
- // ansNum = +ans
-    // console.log(ans)
-    // console.log(fired_button)
-    // if (ans !== myQuestions[questionNumber].correctAnswer) {
-    //     console.log('Hello')
-    //     questionNumber + 1
-
-    // }
-
-    // if (isNaN(ansNum) === false){
-    //     console.log('hiya')
-    //     console.log(ansNum)
-    //     console.log(typeof ansNum)
-    //     // checkRightAnswer(); //oops
-    // } else {
-    //     console.log(ansNum)
-    // }
-
-    // var score = []
-// var initials = $(".highscore-input")
-
-// function saveHighScore() {
-//     var scoresList = {
-//         initials: initials.value,
-//         score: userScore
-//     }
-//     localStorage.setItem("leaderboard", JSON.stringify(scoresList));
-
-
-//     $("#save").on("click", function(event) {
-//         event.preventDefault();
-//         saveHighScore();
-//         console.log('hello')
-//     })
-    
-
-// }
--- assets/highscores.js
+++ assets/highscores.js
@@ -1,31 +1,37 @@
-scoreList = $('#score-list')
-
+var scoreList = $('#score-list')
+var reset = $(".card")
 highscoreList = JSON.parse(localStorage.getItem("Scores"))
-
 
 function renderScores() {
 
-    scoreList.innerHTML = "";
+  scoreList.innerHTML = "";
+  for (var i = 0; i < highscoreList.length; i++) {
+    var score = highscoreList[i];
 
-    for (var i = 0; i < highscoreList.length; i++) {
-      var score = highscoreList[i];
-  
-      var li = document.createElement("li");
-      li.textContent = score;
-      li.setAttribute("data-index", i);
-  
-      scoreList.append(li);
-    }
+    var li = document.createElement("li");
+    li.textContent = score[0] + ": " + score [1];
+    li.setAttribute("data-index", i);
+
+    scoreList.append(li);
+  }
 }
 
 function init() {
-  
-    var storedScores = JSON.parse(localStorage.getItem("Scores"));
-    if (storedScores !== null) {
-      scores = storedScores;
-    }
-    renderScores();
+
+  var storedScores = JSON.parse(localStorage.getItem("Scores"));
+  if (storedScores !== null) {
+    scores = storedScores;
+  }
+  renderScores();
 }
-  
+
+function clearLeaderboard() {
+  window.localStorage.clear();
+  location.reload();
+}
+
+reset.on("click", function(){
+  clearLeaderboard();
+})
 
 init();
